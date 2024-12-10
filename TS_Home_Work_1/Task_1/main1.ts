3d4ac51a032455452fe4f2f27792cca4cf91da5a
Maietskyi <damaietskyi@gmail.com>
2024-12-10 20:55:38 +0200

    Add TypeScript  Home Work Task 1

---
+++ TS_Home_Work_1/Task_1/main1.ts
@@ -0,0 +1,42 @@
+// #dYQNrBV
+// Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+//     Вивести кожну змінну за допомогою: console.log
+
+let hello: string = 'hello';
+console.log(hello);
+
+let owu: string = 'owu';
+console.log(owu);
+
+let com: string = 'com';
+console.log(com);
+
+let ua: string = 'ua';
+console.log(ua);
+
+let a1: number = 1;
+console.log(a1);
+
+let a2: number = 10;
+console.log(a2);
+
+let a3: number = -999;
+console.log(a3);
+
+let a4: number = 123;
+console.log(a4);
+
+let a5: number = 3.14;
+console.log(a5);
+
+let a6: number = 2.7;
+console.log(a6);
+
+let a7: number = 16;
+console.log(a7);
+
+let a8: boolean = true;
+console.log(a8);
+
+let a9: boolean = false;
+console.log(a9);
