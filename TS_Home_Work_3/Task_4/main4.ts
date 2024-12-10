3d77946aacdaddafdb13e174707054b2dcd1d0ba
Maietskyi <damaietskyi@gmail.com>
2024-12-11 00:40:00 +0200

    Add TypeScript Home Work 3 Task 4

---
+++ TS_Home_Work_3/Task_4/main4.ts
@@ -0,0 +1,8 @@
+//     #OeT7t3uUMFi
+// - За допомогою циклу while вивести в документ 20 блоків h1 c довільним текстом і індексом всередині.
+
+let i: number =0;
+while (i < 20) {
+    document.write(`<div><h1>Of course, everything is fine ${i}</h1></div>`);
+    i++;
+}
