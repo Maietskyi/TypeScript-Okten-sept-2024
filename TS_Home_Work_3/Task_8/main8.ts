5f7c80e775014b7dae78a09e463e72ffea770214
Maietskyi <damaietskyi@gmail.com>
2024-12-11 01:09:44 +0200

    Add TypeScript Home Work 3 Task 8

---
+++ TS_Home_Work_3/Task_8/main8.ts
@@ -0,0 +1,15 @@
+// #WpkK0ZH1
+// --створити масив з:
+//     - з 5 числових значень
+// - з 5 стічкових значень
+// - з 5 значень стрічкового, числового та булевого типу
+// - та вивести його в консоль
+
+let numbers: number[] = [55, 98, 35, 78, 12];
+console.log(numbers);
+
+let strings: string[] = ['Okten', 'school', 'Karpaty', 'Lviv', 'Home Work'];
+console.log(strings);
+
+let mix: unknown = ['Okten', '321845', true, 658, false];
+console.log(mix);
