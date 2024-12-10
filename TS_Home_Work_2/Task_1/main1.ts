7641bb3d6432ea996a77d8af48bcd74b095076fe
Maietskyi <damaietskyi@gmail.com>
2024-12-10 21:55:05 +0200

    Add TypeScript Home Work 2 Task 1

---
+++ TS_Home_Work_2/Task_1/main1.ts
@@ -0,0 +1,14 @@
+// #67kfznmiMl
+// - Створити масив, наповнити його 10 елементами будь-якого типу, вивести кожен елемент в консоль
+
+let list: (string | number | boolean)[] = ["Misha", 'Mykhailo', 'Maietskyi', 19, 1.556, 'Lviv', true, '1256', "FC CL - 1968", -58.466, 'Victori'];
+console.log(list[0]);
+console.log(list[1]);
+console.log(list[2]);
+console.log(list[3]);
+console.log(list[4]);
+console.log(list[5]);
+console.log(list[6]);
+console.log(list[7]);
+console.log(list[8]);
+console.log(list[9]);
