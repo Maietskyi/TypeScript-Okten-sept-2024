34563f8a4ba8e870474082ae622e50c2ffaa8a55
Maietskyi <damaietskyi@gmail.com>
2024-12-11 00:32:52 +0200

    Add TypeScript Home Work 3 Task 2

---
+++ TS_Home_Work_3/Task_2/main2.ts
@@ -0,0 +1,6 @@
+// #TYj7ncx
+// - За допомогою циклу for і document.write() вивести 10 блоків div c довільним текстом і індексом всередині
+
+for (let i:number = 0; i < 10; i++) {
+    document.write(`<div><h3>Of course, everything is fine ${i}</h3></div>`)
+}
