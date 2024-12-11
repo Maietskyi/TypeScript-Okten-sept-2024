3f11864ea6e3f4946303215eded1a9b15484060a
Maietskyi <damaietskyi@gmail.com>
2024-12-11 16:29:56 +0200

    Add TypeScript Home Work 4 Task 5

---
+++ TS_Home_Work_4/Task_5/main5.ts
@@ -0,0 +1,8 @@
+// #59g0IsA
+// - створити функцію яка створює параграф з текстом та виводить його через document.write. Текст задати через аргумент
+
+function paragraph (text: string): void {
+    document.write(`<p>${text}</p>`);
+}
+
+paragraph ('Okten school');
