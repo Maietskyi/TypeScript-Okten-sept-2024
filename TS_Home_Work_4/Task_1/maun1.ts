5c0b4557247c8440b0c17465298943bfa668dda4
Maietskyi <damaietskyi@gmail.com>
2024-12-11 13:44:06 +0200

    Add TypeScript Home Work 4 Task 1

---
+++ TS_Home_Work_4/Task_1/maun1.ts
@@ -0,0 +1,17 @@
+// #I2XsG6f
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// 1 варіант
+function raincoat(a:number,b:number):number{
+      return a * b;
+}
+console.log(raincoat(5,8));
+
+// 2 варіант
+function raincoat2(a:number,b:number):number{
+    const result:number = a * b;
+    console.log(result);
+    return result;
+}
+
+raincoat2(7,9);
