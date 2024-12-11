afeafe11290d06b0a1f9e4304a46268c28b3ab25
Maietskyi <damaietskyi@gmail.com>
2024-12-11 14:00:47 +0200

    Add TypeScript Home Work 4 Task 4

---
+++ TS_Home_Work_4/Task_4/main4.ts
@@ -0,0 +1,10 @@
+// #SIdMd0hQ
+// - створити функцію яка приймає масив та виводить кожен його елемент
+
+function arraySearch (array: any): any {
+    for (const item of array){
+        console.log(item);
+    }
+}
+
+arraySearch ([1, 'Lesson', '584', true, "Task"])
