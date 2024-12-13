ffb823cdd89de171a0d94d95bd1810116df886cd
Maietskyi <damaietskyi@gmail.com>
2024-12-13 21:02:48 +0200

    Add TypeScript Home Work 6 Task 1

---
+++ TS_Home_Work_6/Task_1/main1.ts
@@ -0,0 +1,27 @@
+// #dFeorS3m7u
+// - Знайти та вивести довижину настипних стрінгових значень
+// 'hello world', 'lorem ipsum', 'javascript is cool'
+
+let string1: string = 'hello world';
+console.log(string1.length);
+let string2: string = 'lorem ipsum';
+console.log(string2.length);
+let string3: string = 'javascript is cool';
+console.log(string3.length);
+
+// 2 варіант
+const arrayStrings: string[] = ['hello world', 'lorem ipsum', 'javascript is cool'];
+for (const string of arrayStrings) {
+    console.log(string.length)
+}
+
+// 3 варіант з функцією - не впевнений що така конструкція вірна
+
+const stringsArray: string[] = ['hello world', 'lorem ipsum', 'javascript is cool'];
+let stringLength = (strings: string[]): void => {
+    for (const str of strings) {
+        console.log(str.length)
+    }
+}
+stringLength(stringsArray)
+
