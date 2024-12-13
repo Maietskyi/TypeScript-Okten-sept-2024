3976aba1f3be8355da62507b1c4ca9959a632cf0
Maietskyi <damaietskyi@gmail.com>
2024-12-13 18:12:46 +0200

    Add TypeScript Home Work 5 Task 1

---
+++ TS_Home_Work_5/Task_1/main1.ts
@@ -0,0 +1,13 @@
+// #I2XsG6f
+// - створити функцію яка обчислює та повертає площу прямокутника зі сторонами а і б
+
+// 1 варіант
+let raincoat1 = (a: number, b: number): number => a * b;
+
+console.log(raincoat1(5, 8));
+
+// 2 варіант
+
+let raincoat4 = (a: number, b: number): void => console.log(a * b);
+
+raincoat4(7, 9);
