0a02a3de7a533f8c38333ceff762216c0e0f76fb
Maietskyi <damaietskyi@gmail.com>
2024-12-13 09:49:17 +0200

    Add TypeScript Home Work 3 Task 7

--- TS_Home_Work_3/Task_7/main7.ts
+++ TS_Home_Work_3/Task_7/main7.ts
@@ -17,13 +17,13 @@
 //     - користувачів зі статусом true
 // - користувачів зі статусом false
 // - користувачів які старші за 30 років
-class userClass {
+interface Iusers {
     name: string;
     age: number;
     status: boolean
 }
 
-let users: userClass[] = [
+let users: Iusers[] = [
     {name: 'vasya', age: 31, status: false},
     {name: 'petya', age: 30, status: true},
     {name: 'kolya', age: 29, status: true},
@@ -55,6 +55,58 @@ for (const user of users) {
 
 //      - користувачів які старші за 30 років
 for (const user of users) {
+    if (user.age > 30) {
+        console.log(`age > 30`)
+        console.log(user)
+    }
+}
+
+
+// Також можна зробити за допомогою класу
+class UserClass2 {
+    public name: string;
+    public age: number;
+    public status: boolean
+
+    constructor(name: string, age: number, status: boolean) {
+        this.name = name;
+        this.age = age;
+        this.status = status
+    }
+}
+
+let users2: UserClass2[] = [
+    new UserClass2('vasya', 31, false),
+    new UserClass2('petya', 30, true),
+    new UserClass2('kolya', 29, true),
+    new UserClass2('olya', 28, false),
+    new UserClass2('max', 30, true),
+    new UserClass2('anya', 31, false),
+    new UserClass2('oleg', 28, false),
+    new UserClass2('andrey', 29, true),
+    new UserClass2('masha', 30, true),
+    new UserClass2('olya', 31, false),
+    new UserClass2('max', 31, true)
+];
+
+//     - користувачів зі статусом true
+for (const user of users2) {
+    if (user.status === true) {
+        console.log(`status === true`)
+        console.log(user)
+    }
+}
+
+//      - користувачів зі статусом false
+for (const user of users) {
+    if (user.status === false) {
+        console.log(`status === false`)
+        console.log(user)
+    }
+}
+
+//      - користувачів які старші за 30 років
+for (const user of users2) {
     if (user.age > 30) {
         console.log(`age > 30`)
         console.log(user)
