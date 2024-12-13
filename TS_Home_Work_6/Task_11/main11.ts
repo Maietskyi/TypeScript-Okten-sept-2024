9a8120108d9aae1d5e90b2a795794b8d46c7df5a
Maietskyi <damaietskyi@gmail.com>
2024-12-13 23:36:30 +0200

    Add TypeScript Home Work 6 Task 11

---
+++ TS_Home_Work_6/Task_11/main11.ts
@@ -0,0 +1,70 @@
+// #4LJn7zBx
+// взяти з arrays.js масив coursesArray
+// --написати пошук всіх об'єктів, в яких в modules є sass
+// --написати пошук всіх об'єктів, в яких в modules є docker
+
+class CoursesArray {
+    public title: string;
+    public monthDuration: number;
+    public hourDuration: number;
+    public modules: string[]
+
+    constructor(title: string, monthDuration: number, hourDuration: number, modules: string[]) {
+        this.title = title
+        this.monthDuration = monthDuration;
+        this.hourDuration = hourDuration;
+        this.modules = modules
+    }
+
+}
+
+let coursesArray: CoursesArray[] = [
+    {
+        title: 'JavaScript Complex',
+        monthDuration: 5,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'node.js']
+    },
+    {
+        title: 'Java Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular',
+            'aws', 'docker', 'git', 'java core', 'java advanced']
+    },
+    {
+        title: 'Python Complex',
+        monthDuration: 6,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'angular',
+            'aws', 'docker', 'python core', 'python advanced']
+    },
+    {
+        title: 'QA Complex',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'git', 'QA/QC']
+    },
+    {
+        title: 'FullStack',
+        monthDuration: 7,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular',
+            'aws', 'docker', 'git', 'node.js', 'python', 'java']
+    },
+    {
+        title: 'Frontend',
+        monthDuration: 4,
+        hourDuration: 909,
+        modules: ['html', 'css', 'js', 'mysql', 'mongodb', 'react', 'angular', 'aws', 'docker', 'git', 'sass']
+    }
+];
+
+console.log(coursesArray.filter((course: CoursesArray): boolean => {
+    return course.modules.some((module: string): boolean => module === 'sass')
+}));
+
+console.log(coursesArray.filter((course: CoursesArray): boolean => {
+    return course.modules.some((module: string): boolean => module === 'docker')
+}));
+
